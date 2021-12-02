5b1fb8f9f4db3ff135c8f4746cc094495e7c3072
Shuo Chen <shuo.chen@epigenesys.org.uk>
2021-12-02 11:07:52 +0000

    updated visibility map to allow elements only showing if all conditions are met

--- src/visibility_map.js
+++ src/visibility_map.js
@@ -9,6 +9,8 @@ export default class VisibilityMap {
     this.map = JSON.parse(element.dataset.visibilityMap);
   }
 
+  static hiddenBy = new Map();
+
   static start() {
     document.addEventListener('change', (event) => {
       const { target } = event;
@@ -41,20 +43,39 @@ export default class VisibilityMap {
     });
   }
 
+  toggleElementVisibility = (targetElement, visibility) => {
+    const { element } = this;
+    const hiddenByForElement = VisibilityMap.hiddenBy.get(targetElement) || new Set();
+
+    if (visibility) {
+      hiddenByForElement.delete(element);
+    } else {
+      hiddenByForElement.add(element);
+    }
+
+    VisibilityMap.hiddenBy.set(targetElement, hiddenByForElement);
+
+    if (visibility && (hiddenByForElement.size === 0 || !targetElement.dataset.visibleWhenAllConditionsMet)) {
+      toggleVisibility(targetElement, true);
+      targetElement.dispatchEvent(new Event('visibility.show', { bubbles: true }));
+    } else {
+      toggleVisibility(targetElement, false);
+      targetElement.dispatchEvent(new Event('visibility.hide', { bubbles: true }));
+    }
+  }
+
   updateVisibility() {
-    const { action } = this;
+    const { action, toggleElementVisibility } = this;
     const { inputsForValue, otherInputs } = this.getInputsForValues();
 
     const showingInputsForValue = action === 'show';
 
     inputsForValue.forEach((element) => {
-      toggleVisibility(element, showingInputsForValue);
-      element.dispatchEvent(new Event(`visibility.${showingInputsForValue ? 'show' : 'hide'}`, { bubbles: true }));
+      toggleElementVisibility(element, showingInputsForValue);
     });
 
     otherInputs.forEach((element) => {
-      toggleVisibility(element, !showingInputsForValue);
-      element.dispatchEvent(new Event(`visibility.${showingInputsForValue ? 'hide' : 'show'}`, { bubbles: true }));
+      toggleElementVisibility(element, !showingInputsForValue);
     })
   }
 
